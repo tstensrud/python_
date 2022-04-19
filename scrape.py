edc17b2377e2ba5a383e5ed1bfbf96c5550db7e0
tstensrud <99834487+tstensrud@users.noreply.github.com>
2022-04-19 13:10:21 +0200

    Add files via upload
    Added option to download images from entered URL

--- scrape.py
+++ scrape.py
@@ -1,56 +1,91 @@
 import re
 import tkinter
-from tkinter import messagebox
 import requests
-import sys
+
+from tkinter import messagebox
 from tkinter import *
 from bs4 import BeautifulSoup
 
-def writeToFile(input):
+imagesList= []
+
+def writeToFile(input, writeContent):
     fileName = fldUrl.get()
+    fileNameContent = ""
+    if (writeContent == 1):
+        fileNameContent = "links"
+    elif(writeContent == 2):
+        fileNameContent = "emails"
+    elif(writeContent == 3):
+        fileNameContent = "images"
 
     #filename is url without https://, e.g. "www.google.com.txt"
     try:
-        file = open(fileName[8:] + ".txt", "x")
+        file = open(fileName[8:] + " - " + fileNameContent + ".txt", "x")
         file.write(input)
         file.close()
         messagebox.showinfo(title="File written", message="Written to file success!")
     except Exception as e:
         messagebox.showerror(title="File allready exist", message=e.__class__)
 
-def scrapeLinks():
-    pass
-
-def scrapeEmails():
-    pass
+def downloadImages():
+    try:
+        for url in imagesList:
+            imageName = ""
+            imageUrl = requests.get(url) #this fails
+            imageFile = imageUrl
+            for i in reversed(url):
+                if (i == "/"):
+                    break
+                imageName += imageName.join(i)
+            file = open(imageName[::-1], "wb")
+            file.write(imageFile.content)
+            file.close()
+    except Exception as e:
+        messagebox.showerror(title="Could not download", message=e.__class__ )
+    
 
 def scraper():
     txtResult.delete(1.0, tkinter.END)
     write = writeToFileValue.get()
+    downloadImagesChecked = downloadImagesValue.get()
     scrapeType = radioScrapeChoice.get()
+
     #check that URL starts with https://
     url = fldUrl.get()
     if ("https://" not in url):
         txtResult.insert(1.0, "URL \"" + url + "\" is not valid. Must start with https://")
         return
-
-    data = requests.get(url)
-    soup = BeautifulSoup(data.text, 'html.parser')
-    try:
-        if(scrapeType == 1):
-            for i in soup.find_all('a', attrs={'href': re.compile("^https://")}):
-                txtResult.insert(END, "- " + i.get('href') + "\n")
-        elif(scrapeType == 2):
-            for i in soup.find_all('a', attrs={'href': re.compile("^mailto")}):
-                txtResult.insert(END, "- " + i.get('href') + "\n")
-        else:
-            messagebox.showinfo(title="Choose", message="Choose what to scrape")
-    except Exception as e:
-        txtResult.insert(1.0, e.__class__)
     
+    #exception handling if URL connection fails
+    try:
+        data = requests.get(url)
+        soup = BeautifulSoup(data.text, 'html.parser')
+    except Exception as e:
+        messagebox.showerror(title="Connection error", message=e.__class__)
+        return
+
+    #scrape and print to text area
+    #scrapeType 1=links, 2=emails, 3=images
+    
+    if(scrapeType == 1):
+        for i in soup.find_all('a', attrs={'href': re.compile("^http")}):
+            txtResult.insert(END, i.get('href') + "\n")
+    elif(scrapeType == 2):
+        for i in soup.find_all('a', attrs={'href': re.compile("^mailto")}):
+            txtResult.insert(END, i.get('href') + "\n") 
+    elif(scrapeType == 3):
+        imagesList.clear()
+        for i in soup.find_all('img', attrs={'src': re.compile("^")}):
+            imageUrl = i.get('src')
+            txtResult.insert(END, imageUrl + "\n")
+            imagesList.append(imageUrl)
+        if (downloadImagesChecked == 1):
+            downloadImages()
+    else:
+        messagebox.showinfo(title="Choose", message="Choose what to scrape")
     #if write to file is checked
     if (write == 1):
-        writeToFile(txtResult.get(1.0, tkinter.END))
+        writeToFile(txtResult.get(1.0, tkinter.END), scrapeType)
 
 
 
@@ -64,6 +99,7 @@ lblHeader.place(x=20, y=20)
 
 #field for URL-entry
 fldUrl = tkinter.Entry(width="70")
+fldUrl.insert(0, "https://www.structor.no")
 fldUrl.place(x= 50, y = 20)
 
 #button to initiate scrape
@@ -72,15 +108,22 @@ btnUrl.place(x=500, y=17)
 
 #radiobuttons
 radioScrapeChoice = tkinter.IntVar()
-radioBtnLinks = Radiobutton(frmMain, text="Scrape links", variable=radioScrapeChoice, value=1)
+radioBtnLinks = Radiobutton(frmMain, text="Get links", variable=radioScrapeChoice, value=1)
 radioBtnLinks.place(x=20, y=80)
-radioBtnEmails = Radiobutton(frmMain, text="Scrape emails", variable=radioScrapeChoice, value=2)
+radioBtnEmails = Radiobutton(frmMain, text="Get emails", variable=radioScrapeChoice, value=2)
 radioBtnEmails.place(x=20, y=100)
+radioBtnImages = Radiobutton(frmMain, text="Get images", variable=radioScrapeChoice, value=3)
+radioBtnImages.place(x=20, y=120)
 
 #write to file checkbox
 writeToFileValue = tkinter.IntVar()
 chkWrite = tkinter.Checkbutton(frmMain, text="Write to file", variable=writeToFileValue)
 chkWrite.place(x=20, y=60)
+
+#download images
+downloadImagesValue = tkinter.IntVar()
+chkDownloadImages = tkinter.Checkbutton(frmMain, text="Download images", variable=downloadImagesValue)
+chkDownloadImages.place(x=120, y=120)
 
 #scroolbar and text area for output
 scrollbar = tkinter.Scrollbar(frmMain)
